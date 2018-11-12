deaf4611c8d7609ff98eb438de7560d489e9e0d3
voducmanh963 <voducmanh963@gmail.com>
2018-11-12 13:09:32 +0700

    add

--- scripts/core/main.js
+++ scripts/core/main.js
@@ -2,6 +2,32 @@ console.log('%cCANHCAM', 'font-size:100px;color:#ff451a;text-shadow:0 1px 0 #f73
 console.log('%c CANHCAM ' + '%c - The best Web Solutions Provider', 'border-radius: 2px; padding: 3px; background: #ff451a; color: #FFF', 'color: #ff451a');
 console.warn("CANHCAM warning: This is a browser feature intended for developers. If someone told you to copy and paste something here to enable a CANHCAM feature or 'hack' someone's account, it is a scam and will give them access to your CANHCAM account.");
 document.onkeyup = function (a) { if ((a = a || window.event).altKey && a.ctrlKey && a.shiftKey && 13 == a.which) return $("body"), alert(b64DecodeUnicode("QkFPIE5HVVlFTiAtIDA5Njk2ODk4OTMKRW1haWw6IGJhb25ndXllbnlhbUBnbWFpbC5jb20KV2ViOiBiYW9uZ3V5ZW55YW0uZ2l0aHViLmlv")), !1 };
+
 $(function() {
-    AOS.init();
+	AOS.init();
+	AOS.init({
+		// Global settings:
+		disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+		startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+		initClassName: 'aos-init', // class applied after initialization
+		animatedClassName: 'aos-animate', // class applied on animation
+		useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+		disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+		debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+		throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+		
+	  
+		// Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+		offset: 120, // offset (in px) from the original trigger point
+		delay: 0, // values from 0 to 3000, with step 50ms
+		duration: 400, // values from 0 to 3000, with step 50ms
+		easing: 'ease', // default easing for AOS animations
+		once: false, // whether animation should happen only once - while scrolling down
+		mirror: false, // whether elements should animate out while scrolling past them
+		anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+	  
+	  });
+	window.addEventListener('load', function() {
+		AOS.refresh();
+	});
 });
